e700ff24a5588cb97c4f053da85e9113aa88a247
Amelie Schuh <Amelie.Celina.Schuh@gmail.com>
2026-06-01 23:28:43 +0200

    implemented new requirement

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -19,6 +19,9 @@ function update_quality() {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
+          if (items[i].name == 'Conjured Mana Cake') {
+            items[i].quality = items[i].quality - 1
+          }
         }
       }
     } else {
@@ -46,6 +49,9 @@ function update_quality() {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              items[i].quality = items[i].quality - 1
+            }
+            if (items[i].name == 'Conjured Mana Cake') {
               items[i].quality = items[i].quality - 1
             }
           }
